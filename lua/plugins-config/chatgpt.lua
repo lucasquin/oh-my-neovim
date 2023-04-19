b40971bc6eaaf7761496b0f00314ecba8752c1c8
Lucas Quintino <lucas.quintino@pontoid.com.br>
2023-04-19 13:45:24 -0300

    chore: max tokens modified

--- lua/plugins-config/chatgpt.lua
+++ lua/plugins-config/chatgpt.lua
@@ -48,7 +48,7 @@ chatgpt.setup({
 		model = "gpt-3.5-turbo",
 		frequency_penalty = 0,
 		presence_penalty = 0,
-		max_tokens = 300,
+		max_tokens = 3000,
 		temperature = 0,
 		top_p = 1,
 		n = 1,
