1c0fc0a4d55cd0517f21bb4514ad7882dd1cc095
Lucas Quintino <lucas.quintino@pontoid.com.br>
2023-07-03 09:33:32 -0300

    feat(lsp): goto implementation

--- lua/plugins/config/lsp/lspconfig.lua
+++ lua/plugins/config/lsp/lspconfig.lua
@@ -30,8 +30,9 @@ local on_attach = function(client, bufnr)
 
 	local opts = { noremap = true, silent = true, buffer = bufnr }
 	vim.keymap.set("n", "<F12>", "<cmd>Lspsaga peek_definition<CR>", opts) -- go to implementation
-	vim.keymap.set("n", "<C-F12>", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
+	vim.keymap.set("n", "<C-F12>", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- show definition, references
 	vim.keymap.set("n", "<S-F12>", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+	vim.keymap.set("n", "<C-S-F12>", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show documentation for what is under cursor
 	vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
 	vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
 	vim.keymap.set("n", "<leader>fm", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", {}) -- format
