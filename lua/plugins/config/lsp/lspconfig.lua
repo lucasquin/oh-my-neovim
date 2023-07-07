b3949e07302f07b1607b72ba2c339eb280ef9516
Lucas Quintino <lucas.quintino@pontoid.com.br>
2023-07-07 17:33:31 -0300

    chore: Omnisharp little refactor

--- lua/plugins/config/lsp/lspconfig.lua
+++ lua/plugins/config/lsp/lspconfig.lua
@@ -13,19 +13,20 @@ if not typescript_setup then
 	return
 end
 
-local on_attach = function(client, bufnr)
-	client.server_capabilities.documentFormattingProvider = false
+local function toSnakeCase(str)
+	return string.gsub(str, "%s*[- ]%s*", "_")
+end
 
-	if not client.server_capabilities.semanticTokensProvider then
-		local semantic = client.config.capabilities.textDocument.semanticTokens
-		client.server_capabilities.semanticTokensProvider = {
-			full = true,
-			legend = {
-				tokenTypes = semantic.tokenTypes,
-				tokenModifiers = semantic.tokenModifiers,
-			},
-			range = true,
-		}
+local on_attach = function(client, bufnr)
+	if client.name == "omnisharp" then
+		local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
+		for i, v in ipairs(tokenModifiers) do
+			tokenModifiers[i] = toSnakeCase(v)
+		end
+		local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
+		for i, v in ipairs(tokenTypes) do
+			tokenTypes[i] = toSnakeCase(v)
+		end
 	end
 
 	local opts = { noremap = true, silent = true, buffer = bufnr }
@@ -104,85 +105,15 @@ typescript.setup({
 lspconfig["omnisharp"].setup({
 	analyze_open_documents_only = true,
 	capabilities = capabilities,
-	enable_editorconfig_support = true,
+	enable_editorconfig_support = false,
 	enable_import_completion = false,
 	enable_ms_build_load_projects_on_demand = false,
 	enable_roslyn_analyzers = false,
 	organize_imports_on_format = false,
 	sdk_include_prereleases = true,
 	filetypes = { "cs", "vb" },
-	on_attach = function(client, bufnr)
-		on_attach(client, bufnr)
-		client.server_capabilities.semanticTokensProvider.legend = {
-			tokenModifiers = { "static" },
-			tokenTypes = {
-				"comment",
-				"excluded_code",
-				"identifier",
-				"keyword",
-				"keyword_control",
-				"number",
-				"operator",
-				"operator_overloaded",
-				"preprocessor_keyword",
-				"string",
-				"whitespace",
-				"text",
-				"static_symbol",
-				"preprocessor_text",
-				"punctuation",
-				"string_verbatim",
-				"string_escape_character",
-				"class_name",
-				"delegate_name",
-				"enum_name",
-				"interface_name",
-				"module_name",
-				"struct_name",
-				"type_parameter_name",
-				"field_name",
-				"enum_member_name",
-				"constant_name",
-				"local_name",
-				"parameter_name",
-				"method_name",
-				"extension_method_name",
-				"property_name",
-				"event_name",
-				"namespace_name",
-				"label_name",
-				"xml_doc_comment_attribute_name",
-				"xml_doc_comment_attribute_quotes",
-				"xml_doc_comment_attribute_value",
-				"xml_doc_comment_cdata_section",
-				"xml_doc_comment_comment",
-				"xml_doc_comment_delimiter",
-				"xml_doc_comment_entity_reference",
-				"xml_doc_comment_name",
-				"xml_doc_comment_processing_instruction",
-				"xml_doc_comment_text",
-				"xml_literal_attribute_name",
-				"xml_literal_attribute_quotes",
-				"xml_literal_attribute_value",
-				"xml_literal_cdata_section",
-				"xml_literal_comment",
-				"xml_literal_delimiter",
-				"xml_literal_embedded_expression",
-				"xml_literal_entity_reference",
-				"xml_literal_name",
-				"xml_literal_processing_instruction",
-				"xml_literal_text",
-				"regex_comment",
-				"regex_character_class",
-				"regex_anchor",
-				"regex_quantifier",
-				"regex_grouping",
-				"regex_alternation",
-				"regex_text",
-				"regex_self_escaped_character",
-				"regex_other_escape",
-			},
-			range = true,
-		}
-	end,
+	on_attach = on_attach,
+	flags = {
+		debounce_text_changes = 150,
+	},
 })
