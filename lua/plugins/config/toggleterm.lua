c5783e13704e27255fe39022dc47ecf60555b674
Lucas Quintino <lucas.quintino@pontoid.com.br>
2023-08-18 10:36:15 -0300

    fix(toggleterm): keymaps function

--- lua/plugins/config/toggleterm.lua
+++ lua/plugins/config/toggleterm.lua
@@ -28,11 +28,15 @@ toggleterm.setup({
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<C-t>", "<cmd>Neotree toggle<CR><cmd>ToggleTerm<CR><cmd>Neotree<CR>", opts)
 
-local opt = { buffer = 0 }
-vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opt)
-vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opt)
-vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opt)
-vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opt)
-vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opt)
-vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opt)
-vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opt)
+function _G.set_terminal_keymaps()
+  local opt = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opt)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opt)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opt)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opt)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opt)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opt)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opt)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
