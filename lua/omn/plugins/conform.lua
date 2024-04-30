145134b1b073727cd8f48ec891692549b6e1f53f
lucasquin <lucaslquintino@gmail.com>
2024-04-30 14:24:22 -0300

    feat: formatter for svelte files added

--- lua/omn/plugins/conform.lua
+++ lua/omn/plugins/conform.lua
@@ -1,25 +1,25 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require("conform").setup {
-      formatters_by_ft = {
-        javascript = { "prettierd" },
-        typescript = { "prettierd" },
-        javascriptreact = { "prettierd" },
-        typescriptreact = { "prettierd" },
-        svelte = { "prettierd" },
-        css = { "prettierd" },
-        html = { "prettierd" },
-        json = { "prettierd" },
-        yaml = { "prettierd" },
-        graphql = { "prettierd" },
-        markdown = { "markdownlint" },
-        lua = { "stylua" },
-        go = { "gofumpt", "goimports" },
-        robot = { "robotframework_ls" },
-        sh = { "shfmt" },
-      },
-    }
-  end,
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				go = { "gofumpt", "goimports" },
+				robot = { "robotframework_ls" },
+				sh = { "shfmt" },
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				css = { "prettierd" },
+				html = { "prettierd" },
+				json = { "prettierd" },
+				yaml = { "prettierd" },
+				graphql = { "prettierd" },
+				markdown = { "prettierd" },
+				svelte = { "prettierd --plugin prettier-plugin-svelte" },
+			},
+		})
+	end,
 }
