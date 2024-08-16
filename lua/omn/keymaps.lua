b60bbba989757939cebcefc3e3097f47376ce5c8
lucasquin <lucaslquintino@gmail.com>
2024-08-16 16:56:20 -0300

    chore: refactor in keymaps

--- lua/omn/keymaps.lua
+++ lua/omn/keymaps.lua
@@ -1,118 +1,75 @@
----@diagnostic disable: different-requires
-
-local maps = {
-  Neovim = {
-    n = {
-      ["<Esc>"] = { "<cmd>noh<CR>", "Clear highlights" },
-      ["<C-Up>"] = { "<cmd>resize -2<CR>", "Resize window up" },
-      ["<C-Down>"] = { "<cmd>resize +2<CR>", "Resize window down" },
-      ["<C-Left>"] = { "<cmd>vertical resize -2<CR>", "Resize window left" },
-      ["<C-Right>"] = { "<cmd>vertical resize -2<CR>", "Resize window right" },
-      ["<C-h>"] = { "<C-w>h", "Move to window on the left" },
-      ["<C-j>"] = { "<C-w>j", "Move to window below" },
-      ["<C-k>"] = { "<C-w>k", "Move to window above" },
-      ["<C-l>"] = { "<C-w>l", "Move to window on the right" },
-      ["<C-a>"] = { "ggVG", "Select all text" },
-      ["<C-s>"] = { "<cmd>w<CR>", "Save the current file" },
-      ["<leader>b"] = { "<cmd>enew<CR>", "Create a new buffer" },
-    },
-    i = {
-      ["<C-h>"] = { "<Left>", "Move cursor one character left" },
-      ["<C-j>"] = { "<Down>", "Move cursor one line down" },
-      ["<C-k>"] = { "<Up>", "Move cursor one line up" },
-      ["<C-l>"] = { "<Right>", "Move cursor one character right" },
-      ["<S-Tab>"] = { "<C-d>", "Move one indentation level back" },
-    },
-  },
-  Conform = {
-    n = {
-      ["<leader>fm"] = {
-        function()
-          require("conform").format { lsp_fallback = true, async = false, timeout_ms = 1000 }
-        end,
-        "Format the current buffer",
-      },
-    },
-    v = {
-      ["<leader>fm"] = {
-        function()
-          require("conform").format { lsp_fallback = true, async = false, timeout_ms = 1000 }
-        end,
-        "Range format the current visual selection ",
-      },
-    },
-  },
-  DAP = {
-    n = {
-      ["<F5>"] = {
-        function()
-          require("dap").continue()
-        end,
-        "Continue program execution (if paused)",
-      },
-      ["<S-F5>"] = {
-        function()
-          require("dap").terminate()
-        end,
-        "Terminate the debugging session",
-      },
-      ["<F9>"] = {
-        function()
-          require("dap").toggle_breakpoint()
-        end,
-        "Toggle a breakpoint at the current line",
-      },
-      ["<F10>"] = {
-        function()
-          require("dap").step_over()
-        end,
-        "Step over the next line",
-      },
-      ["<F11>"] = {
-        function()
-          require("dap").step_into()
-        end,
-        "Step into the next function call",
-      },
-      ["<S-F11>"] = {
-        function()
-          require("dap").step_out()
-        end,
-        "Step out of the current function",
-      },
-    },
-  },
-  Telescope = {
-    n = {
-      ["<leader>ff"] = { "<cmd>Telescope find_files<CR>", "Search for files" },
-      ["<leader>fg"] = { "<cmd>Telescope live_grep<CR>", "Search text within files" },
-      ["<leader>fb"] = { "<cmd>Telescope current_buffer_fuzzy_find<CR>", "Fuzzy search within the current buffer" },
-      ["<leader>fh"] = { "<cmd>Telescope help_tags<CR>", "Search help topics" },
-    },
-  },
-  Neotree = {
-    n = {
-      ["<C-n>"] = { "<cmd>Neotree toggle<CR>", "Toggle the 'NvimTree' file explorer sidebar" },
-    },
-  },
-  LSP = {
-    n = {
-      ["<F12>"] = { vim.lsp.buf.definition, "Jump to the definition of the symbol under the cursor" },
-      ["<C-F12>"] = { vim.lsp.buf.declaration, "Jump to the definition of the symbol under the cursor" },
-      ["<C-S-F12>"] = { vim.lsp.buf.implementation, "Jump to the implementation of the symbol under the cursor" },
-      ["<S-F12>"] = { vim.lsp.buf.signature_help, "Show documentation for the symbol under the cursor" },
-      ["<leader>rn"] = { vim.lsp.buf.rename, "Rename the symbol under the cursor" },
-      ["<leader>ca"] = { vim.lsp.buf.code_action, "Execute code actions on the current line(s)" },
-    },
-  },
-}
-
 local map = vim.keymap.set
 
-for _, plugins in pairs(maps) do
-  for mode, keybindings in pairs(plugins) do
-    for keybinding, mapping in pairs(keybindings) do
-      map(mode, keybinding, mapping[1], { desc = mapping[2] })
-    end
-  end
-end
+-- Neotree
+map("n", "<C-n>", "<cmd>Neotree toggle<CR>", { desc = "Neotree toggle window" })
+
+-- Comment
+map("n", "gcc", "gcc", { desc = "Toggle Comment", remap = true })
+map("v", "gc", "gc", { desc = "Toggle comment", remap = true })
+
+-- LSP
+map("n", "<F12>", vim.lsp.buf.definition, { desc = "Jump to the definition of the symbol under the cursor" })
+map("n", "<C-F12>", vim.lsp.buf.declaration, { desc = "Jump to the declaration of the symbol under the cursor" })
+map("n", "<C-S-F12>", vim.lsp.buf.implementation, { desc = "Jump to the implementation of the symbol under the cursor" })
+map("n", "<S-F12>", vim.lsp.buf.signature_help, { desc = "Show documentation for the symbol under the cursor" })
+map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename the symbol under the cursor" })
+map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Execute code actions on the current line(s)" })
+
+-- Telescope
+map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" })
+map("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
+map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "telescope find oldfiles" })
+map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
+map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
+map("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
+map("n", "<leader>ff", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>", { desc = "telescope find all files" })
+map("n", "<leader>fb", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "telescope find in current buffer" })
+
+-- General
+map("n", "<Esc>", "<cmd>noh<CR>", { desc = "Clear highlights" })
+map("n", "<C-Up>", "<cmd>resize -2<CR>", { desc = "Resize window up" })
+map("n", "<C-Down>", "<cmd>resize +2<CR>", { desc = "Resize window down" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Resize window left" })
+map("n", "<C-Right>", "<cmd>vertical resize -2<CR>", { desc = "Resize window right" })
+map("n", "<C-h>", "<C-w>h", { desc = "Move to window on the left" })
+map("n", "<C-j>", "<C-w>j", { desc = "Move to window below" })
+map("n", "<C-k>", "<C-w>k", { desc = "Move to window above" })
+map("n", "<C-l>", "<C-w>l", { desc = "Move to window on the right" })
+map("n", "<C-a>", "ggVG", { desc = "Select all text" })
+map("n", "<C-s>", "<cmd>w<CR>", { desc = "Save the current file" })
+map("n", "<leader>b", "<cmd>enew<CR>", { desc = "Create a new buffer" })
+map("i", "<S-Tab>", "<C-d>", { desc = "Move one indentation level back" })
+
+-- Conform
+map("n", "<leader>fm", function()
+  require("conform").format { lsp_fallback = true, async = false, timeout_ms = 1000 }
+end, { desc = "Format the current buffer" })
+
+map("v", "<leader>fb", function()
+  require("conform").format { lsp_fallback = true, async = false, timeout_ms = 1000 }
+end, { desc = "Range format the current visual selection" })
+
+-- DAP
+map("n", "<F5>", function()
+  require("dap").continue()
+end, { desc = "telescope find in current buffer" })
+
+map("n", "<S-F5>", function()
+  require("dap").terminate()
+end, { desc = "Terminate the debugging session" })
+
+map("n", "<F9>", function()
+  require("dap").toggle_breakpoint()
+end, { desc = "Toggle a breakpoint at the current line" })
+
+map("n", "<F10>", function()
+  require("dap").step_over()
+end, { desc = "Step over the next line" })
+
+map("n", "<F11>", function()
+  require("dap").step_into()
+end, { desc = "Step into the next function call" })
+
+map("n", "<S-F11>", function()
+  require("dap").step_out()
+end, { desc = "Step out of the current function" })
