59cf5d8b68496d712c89c780c612c0cd001e2df3
lucasquin <lucaslquintino@gmail.com>
2024-08-20 14:02:01 -0300

    chore: set tokyonight colorscheme as default

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
   {
     "Mofiqul/dracula.nvim",
-    config = function()
-      vim.cmd [[colorscheme dracula]]
-    end,
+    -- config = function()
+    --   vim.cmd [[colorscheme dracula]]
+    -- end,
   },
   {
     "folke/tokyonight.nvim",
-    -- config = function()
-    --   vim.cmd [[colorscheme tokyonight]]
-    -- end,
+    config = function()
+      vim.cmd [[colorscheme tokyonight]]
+    end,
   },
   {
     "Mofiqul/vscode.nvim",
