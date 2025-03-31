38b44065af5fc4bee318a726b8493928824e3809
lucasquin <lucaslquintino@gmail.com>
2025-03-31 17:54:55 -0300

    chore: improves

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,10 +9,8 @@ return {
         prompt_prefix = "   ",
         selection_caret = " ",
         entry_prefix = " ",
-        sorting_strategy = "ascending",
         layout_config = {
           horizontal = {
-            prompt_position = "top",
             preview_width = 0.55,
           },
           width = 0.87,
