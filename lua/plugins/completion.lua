c84c223ff500bc0a821ab77092dbd89911c258a5
lucasquin <lucaslquintino@gmail.com>
2025-05-20 17:47:28 -0300

    chore: improve performance

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -20,6 +20,9 @@ return {
       nerd_font_variant = "mono",
     },
     completion = {
+      throttle_ms = 50,
+      trigger_min_chars = 2,
+      max_items = 10,
       menu = {
         border = "single",
         winhighlight = "Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:CursorLine,Search:None",
@@ -32,8 +35,13 @@ return {
           border = "single",
         },
         auto_show = true,
-        auto_show_delay_ms = 250,
+        auto_show_delay_ms = 500,
         treesitter_highlighting = true,
+      },
+      cache = {
+        buffer = true,
+        path = true,
+        ttl_seconds = 120,
       },
     },
     sources = {
@@ -41,7 +49,15 @@ return {
     },
     fuzzy = {
       implementation = "prefer_rust_with_warning",
+      matcher = {
+        distance_bonus = true,
+        prefix_bonus_weight = 2.0,
+        distance_penalty_weight = 0.6,
+        consecutive_bonus_weight = 1.5,
+      },
     },
+    async = true,
+    preload_sources = { "lsp" },
   },
   opts_extend = { "sources.default" },
 }
