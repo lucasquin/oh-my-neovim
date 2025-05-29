c555439725912394c824a27ab8ea8790d40909db
lucasquin <lucaslquintino@gmail.com>
2025-05-29 14:50:45 -0300

    fix: remove unused options

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -20,9 +20,6 @@ return {
       nerd_font_variant = "mono",
     },
     completion = {
-      throttle_ms = 50,
-      trigger_min_chars = 2,
-      max_items = 10,
       menu = {
         border = "single",
         winhighlight = "Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:CursorLine,Search:None",
@@ -38,26 +35,13 @@ return {
         auto_show_delay_ms = 500,
         treesitter_highlighting = true,
       },
-      cache = {
-        buffer = true,
-        path = true,
-        ttl_seconds = 120,
-      },
     },
     sources = {
       default = { "lsp", "path", "snippets", "buffer" },
     },
     fuzzy = {
       implementation = "prefer_rust_with_warning",
-      matcher = {
-        distance_bonus = true,
-        prefix_bonus_weight = 2.0,
-        distance_penalty_weight = 0.6,
-        consecutive_bonus_weight = 1.5,
-      },
     },
-    async = true,
-    preload_sources = { "lsp" },
   },
   opts_extend = { "sources.default" },
 }
